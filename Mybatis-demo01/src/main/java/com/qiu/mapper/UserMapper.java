c42554e540706322bba6007c549774b5e366ac65
zhangjinqiu03 <zhangjinqiu03@58.com>
2020-11-11 20:12:40 +0800

    crud

--- Mybatis-demo01/src/main/java/com/qiu/mapper/UserMapper.java
+++ Mybatis-demo01/src/main/java/com/qiu/mapper/UserMapper.java
@@ -1,9 +1,19 @@
 package com.qiu.mapper;
 
 import com.qiu.pojo.User;
+import org.apache.ibatis.annotations.Delete;
 
 import java.util.List;
 
 public interface UserMapper {
-    public List<User> selectUser();
+    public List<User> selectUsers();
+
+    public User selectUser(int id);
+
+    public void addUser(User user);
+
+    public void updateUser(User user);
+
+    @Delete(value = " delete from mybatis.user where id=#{id} ")
+    public void delUser(int id);
 }
--- Mybatis-demo01/src/test/java/TestMybatis.java
+++ Mybatis-demo01/src/test/java/TestMybatis.java
@@ -1,25 +1,58 @@
 import com.qiu.mapper.UserMapper;
 import com.qiu.pojo.User;
 import com.qiu.util.MybatisUtil;
-import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
-import org.apache.ibatis.session.SqlSessionFactory;
-import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import org.junit.Test;
 
 import java.io.IOException;
-import java.io.InputStream;
 import java.util.List;
 
 public class TestMybatis {
     @Test
-    public void testMybatis() throws IOException {
+    public void testUsersMybatis() throws IOException {
         SqlSession sqlSession = MybatisUtil.getSession(true);
         UserMapper mapper = sqlSession.getMapper(UserMapper.class);
-        List<User> userList = mapper.selectUser();
+        List<User> userList = mapper.selectUsers();
 
         for (User user : userList) {
             System.out.println(user);
         }
+    }
+    @Test
+    public void testUserMybatis() throws IOException {
+        SqlSession sqlSession = MybatisUtil.getSession(true);
+        UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+        User userList = mapper.selectUser(1);
+        System.out.println(userList);
+    }
+    @Test
+    public void testAddMybatis() throws IOException {
+        SqlSession sqlSession = MybatisUtil.getSession(true);
+        UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+        mapper.addUser(new User(5,"i","pw"));
+        User user = mapper.selectUser(5);
+        System.out.println(user);
+    }
+
+    @Test
+    public void testDelMybatis() throws IOException {
+        SqlSession sqlSession = MybatisUtil.getSession(true);
+        UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+        mapper.delUser(4);
+        User user = mapper.selectUser(4);
+        System.out.println(user == null);
+    }
+
+    @Test
+    public void testUpdateMybatis() throws IOException {
+        SqlSession sqlSession = MybatisUtil.getSession(true);
+        UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+        User user = mapper.selectUser(4);
+        System.out.println(user);
+        mapper.updateUser(new User(4,"close","update"));
+        sqlSession.close();
+        sqlSession = MybatisUtil.getSession(true);
+        user = sqlSession.getMapper(UserMapper.class).selectUser(4);
+        System.out.println(user);
     }
 }
